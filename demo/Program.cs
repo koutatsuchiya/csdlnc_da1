357b58c3d1790603281f672910acf1e1be477db4
koutatsuchiya <85290205+koutatsuchiya@users.noreply.github.com>
2021-12-17 09:06:13 +0700

    Add files via upload

---
+++ demo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            //cau a, b
+            Application.Run(new Form1());
+
+            //cau c
+            //Application.Run(new Form2());
+        }
+    }
+}
